6a990334f82c27fe3ca9df8cefffdd7efc754dc9
geektimo <397144589@qq.com>
2019-02-25 16:38:36 +0800

    process-express

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="bg-dark text-white mt-5 p-4 text-center">
+        Copyright &copy;2018 geektimo@gmail.com
+    </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+</style>
